e52936d26487d2c9bf59593d108eb8ad7ce081ce
Gabriel <weaselssh@gmail.com>
2025-04-25 08:04:14 -0700

    primer commit

---
+++ src/proyectoprueba/ProyectoPrueba.java
@@ -0,0 +1,10 @@
+package proyectoprueba;
+
+public class ProyectoPrueba {
+
+    public static void main(String[] args) {
+        System.out.println("Oa mundo");
+        System.out.println("Primer repositorio");
+    }
+    
+}
